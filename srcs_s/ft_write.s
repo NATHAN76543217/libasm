c16eb012ddf14262f548097a8befb4eb782d71cf
Nathan <maximenathan@hotmail.fr>
2020-10-10 19:33:56 +0200

    mac os handled

--- srcs_s/ft_write.s
+++ srcs_s/ft_write.s
@@ -10,7 +10,8 @@
 	%define		NAME	ft_write
 	%assign		WRITE_SYSCALL_NB	0x1 
 	%macro		ERRNO_PROCESS	1
-	
+	cmp		rax, 0
+		jns		%1
 	neg		rax							;invert error code
 	%endmacro
 %elifidni	__OUTPUT_FORMAT__, macho64
@@ -19,7 +20,7 @@
 	%define		NAME	_ft_write
 	%assign		WRITE_SYSCALL_NB	0x2000004
 	%macro		ERRNO_PROCESS	1
-	
+	jnc		%1
 	%endmacro
 %endif
 
@@ -31,16 +32,11 @@ NAME:
 	mov     rbp, rsp
 	mov		rax, WRITE_SYSCALL_NB		; write
 	syscall
-	cmp		rax, 0
-		js	error
-	leave
-	ret
-
-error:
-	ERRNO_PROCESS hey
-	mov		rdi, rax					;save it in rdi
-	call	ERRNO	;get errno pointed by rax
+	ERRNO_PROCESS	quit				;if no error jump to quit
+	mov		rdi, rax					;save error code in rdi
+	call	ERRNO						;get errno pointer in rax
 	mov		DWORD [rax], edi			;put error code in errno
 	mov		rax, -1						;put return value -1 in RAX
+quit:
 	leave
 	ret
